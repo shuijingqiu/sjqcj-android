06f30af1c7dc37c8da800e937210c075f77b99f2
mwm0022 <mwm0022@qq.com>
2016-10-20 12:27:33 +0800

    ghjgjg

--- app/build/intermediates/assets/debug/js/main.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
-
-});
-
-
--- app/src/main/java/com/example/sjqcjstock/Activity/stocks/DynamicExpertActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.sjqcjstock.Activity.stocks;
-
-/**
- * 牛人动态列表（不知道要不要）
- * Created by Administrator on 2016/8/16.
- */
-public class DynamicExpertActivity {
-}
